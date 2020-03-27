0b859493f90f9030bab87a8a66f82e1ecbed8946
Torben Leuschner <info@torbenleuschner.de>
2020-03-27 10:34:17 +0100

    Update CockroachGrammar.php

--- src/Cockroach/Grammar/Query/CockroachGrammar.php
+++ src/Cockroach/Grammar/Query/CockroachGrammar.php
@@ -109,7 +109,7 @@ class CockroachGrammar extends Grammar
         // Each one of the columns in the update statements needs to be wrapped in the
         // keyword identifiers, also a place-holder needs to be created for each of
         // the values in the list of bindings so we can make the sets statements.
-        $columns = $this->compileUpdateColumns($values);
+        $columns = $this->compileUpdateColumns($query, $values);
 
         $from = $this->compileUpdateFrom($query);
 
@@ -124,7 +124,7 @@ class CockroachGrammar extends Grammar
      * @param  array   $values
      * @return string
      */
-    protected function compileUpdateColumns($values)
+    protected function compileUpdateColumns(Builder $query, $values)
     {
         // When gathering the columns for an update statement, we'll wrap each of the
         // columns and convert it to a parameter value. Then we will concatenate a
